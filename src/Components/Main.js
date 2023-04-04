a6119b7b19d152304cf987c4e18a8543cc735fac
Sotiris Ntokas <ntokas1995@live.com>
2023-04-04 14:28:14 +0200

    load movies from json and display year

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,38 +1,33 @@
+import movies from "../data/movies.json";
+
 import "./Main.css"
 
 function Main(){
 
-  const moviesArray = [
-      {
-        "id": 1,
-        "title": "The Godfather",
-        "rating": 9
-      },
-      {
-        "id": 2,
-        "title": "Forrest Gump",
-        "rating": 9
-      },
-      {
-        "id": 3,
-        "title": "Gladiator",
-        "rating": 8
-      },
-    ]
+
+    
 
 
-  return(
-      <div className="Main">
-          {moviesArray.map( (movieObj) => {
-              return (
-                <div key={movieObj.id} className="card">
-                    <p>{movieObj.title}</p>
-                    <p>Rating: {movieObj.rating}</p>
-                </div>
-              )
-          })}
-      </div>
-  )
-}
+    return(
+        <div className="Main">
+            {movies.map( (moviesObj) => {
+               return (
+                <div key={moviesObj.id} className="card">
+                    <h2>{moviesObj.title}</h2>
+                    <h3>{moviesObj.year}</h3>
+                    <h3>{moviesObj.rating}</h3>
+                   
+                 </div>
+               )
+            })}
+        </div>
+    )
+          }
+    
+
 
 export default Main;
+
+
+
+ 
